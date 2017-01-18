50a31a2fc1f4143a799d76742ede43f8b6127588
Tom <thomasscully-king@Thomass-MacBook-Pro.local>
2017-01-18 19:46:43 +0000

    Added break timer and additional controls

--- script/countdowntimer.js
+++ script/countdowntimer.js
@@ -1,34 +1,57 @@
 
-function Countdown(min,element){
-
+function Countdown(min, element, breakMin){
+"use strict";
 // var min = 25;
 
 var time = min * 60;
+var breakTime = breakMin * 60;
 var duration = moment.duration(time * 1000, 'milliseconds');
+var duration2 = moment.duration((time * 1000) + 2000, 'milliseconds');
+var breakDuration = moment.duration((breakTime * 1000) + 2000, 'milliseconds');
 var interval = 1000;
+
 var intervalSet;
 var minCounter = 0;
 var number = Math.max(parseInt(element.textContent));
-var number2;
+
+
+var isBreak = false;
+
 
 function countDown(){
+    
   duration = moment.duration(duration.asMilliseconds() - interval, 'milliseconds');
+
   if(duration < 0){
-      clearInterval(intervalSet);
-          intervalSet = null;
-          this.isOn = false;
+      if( isBreak){
+          element.textContent = "Break Time!";
+          duration = moment.duration(breakDuration.asMilliseconds() - interval, 'milliseconds');;
+    //   clearInterval(intervalSet);
+    //       intervalSet = null;
+    //       this.isOn = false;
+           isBreak = false;
+          
+        } else if(!isBreak){
+          element.textContent = "over";
+         
+          duration = moment.duration(duration2.asMilliseconds() - interval, 'milliseconds');
+          isBreak = true;
+        }
+      
   } else {
-  element.textContent = moment(duration.asMilliseconds()).format('mm:ss');
+        element.textContent = moment(duration.asMilliseconds()).format('mm:ss');
   }
 }
 
 
 this.isOn = false;
 
+
 this.start = function(){
     if(!this.isOn){
         intervalSet = setInterval(countDown, interval);
         this.isOn = true;
+        isBreak = true;
     }
 }
 this.stop = function(){
@@ -40,12 +63,14 @@ this.stop = function(){
 }
 this.add = function(){
     duration.add(1, "minutes");
+    duration2.add(1, "minutes");
     minCounter++;
-
+    
     element.textContent = (parseInt(minCounter + number) <= 9 ? "0" + parseInt(minCounter + number) : parseInt(minCounter + number)) + ":00";
 }
 this.minus = function(){
     duration.subtract(1, "minutes");
+    duration2.subtract(1, "minutes");
     minCounter--;
     element.textContent = (parseInt(minCounter + number) <= 9 ? "0" + parseInt(minCounter + number) : parseInt(minCounter + number)) + ":00";
 }
@@ -56,5 +81,22 @@ this.reset = function(){
     this.isOn = false;
     
 }
+
+// this.breakStart = function(){
+//     if(!this.isOn){
+//         intervalSet = setInterval(countDown, interval);
+//         this.isOn = true;
+//     }
+
+this.breakAdd = function(){
+    breakDuration.add(1, "minutes");
+}
+this.breakMinus = function(){
+    breakDuration.subtract(1, "minutes");
+
+}
+
+
+
 
 }
--- script/main.js
+++ script/main.js
@@ -3,11 +3,15 @@ var startButton = document.getElementById('start-button');
 var plus = document.getElementById('plus');
 var minus = document.getElementById('minus');
 var reset = document.getElementById('reset');
+var breakPlus = document.getElementById('break-plus');
+var breakMinus = document.getElementById('break-minus');
+var setBreak = document.getElementById('set-break');
+var setTimer = document.getElementById('set-timer');
 
 var minutes = parseInt(timeDisplay.textContent);
 
 
-var count = new Countdown(minutes,timeDisplay);
+var count = new Countdown(minutes,timeDisplay, 5);
 
 
 
@@ -27,13 +31,31 @@ startButton.addEventListener('click',function(){
 
 plus.addEventListener('click',function(){
      count.add();
-     
+    
+    var number = parseInt(setTimer.textContent);
+    setTimer.textContent = number + 1;
 });
 minus.addEventListener('click',function(){
      count.minus();
+
+     var number = parseInt(setTimer.textContent);
+     setTimer.textContent = number - 1;
 });
 reset.addEventListener('click',function(){
      count.reset();
+});
+
+breakPlus.addEventListener('click', function(){
+    count.breakAdd();
+
+    var number = parseInt(setBreak.textContent);
+    setBreak.textContent = number + 1;
+});
+breakMinus.addEventListener('click', function(){
+    count.breakMinus();
+
+    var number = parseInt(setBreak.textContent);
+    setBreak.textContent = number - 1;
 });
 
 
